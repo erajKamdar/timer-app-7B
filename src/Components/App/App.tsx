cd97b56f9c76bd66f8e17d985ecd087081418a49
Eraj <erajkamdar15@gmail.com>
2020-11-15 20:23:40 +0500

    changes

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Timer from '../Timer/timer'
 import './App.css'
 
+const Name = '<Timer App />'
+
 const App = () => <div className='app-container'>
-    <h3 className='Heading'>Timer App</h3>
+    <h3 className='Heading'>{Name}</h3>
     <Timer />
     <p><a className='footer' href='https://github.com/erajKamdar'>&copy; Eraj Hanif</a></p>
 </div>;
